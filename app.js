fa634d0b3b90776b74c29bc637dc8f8f5e256cb8
TrolessiAgustin <Trolessiagustin02@gmail.com>
2025-02-19 19:04:17 -0300

    Actualiza el proyecto

--- app.js
+++ app.js
@@ -1,39 +1,63 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-/*
-    PASOS DEL AMIGO SECRETO
+// Creo la lista
+let amigos = [];
 
-    1) INGRESAR AMIGOS PARA SORTEAR
-    2) INGRESARLOS EN UN VECTOR
-    3) MOSTRAR LA LISTA EN PANTALLA  
-*/
-listaAmigos = [];
 
+// Creo una funcion para Asignar textos a elementos del apartado HTML
 function asignarElementoTexto(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     }
 
-
+// Desarrollo la funcion para Agregar Amigo
 function añadirAmigo(){
-    let amigoSecreto = document.getElementById(`amigo`).value;
-    listaAmigos.push(` ${amigoSecreto}`);
-    asignarElementoTexto('#listaAmigos', listaAmigos);
+    let amigoSecreto = document.getElementById(`amigo`).value.trim();
+    // el trim al final del document es para limpiar los espacios vacios en el string
+    
+
+    if (amigoSecreto === '') { // Verifico si el nombre ingresado esta vacio
+        alert('Por favor, inserte un nombre.');
+        return;
+    } else if (amigos.includes(amigoSecreto)) { // Verifico si el nombre ingresado esta en la lista
+        alert('Este amigo ya está en la lista.');
+        return;
+    }
+    
+    amigos.push(` ${amigoSecreto}`); // Agrego el amigo a la lista de Amigos
+     document.getElementById("amigo").value = ""; // Se vacia la linea de texto donde insertamos el amigo
+
     console.log(amigoSecreto);
+    actualizarListaAmigos();
 }
 
 function sortearAmigo(){
-    limpiarCaja();
-    asignarElementoTexto('#listaAmigos', "");
-    let sortearAmigo = parseInt(Math.floor(Math.random()*listaAmigos.length));
-    let amigoSorteado = listaAmigos[sortearAmigo];
+
+    let sortearAmigo = parseInt(Math.floor(Math.random()*amigos.length));
+    let amigoSorteado = amigos[sortearAmigo];
     asignarElementoTexto('#resultado', amigoSorteado);
+
+    if(amigos.length === 0){
+        asignarElementoTexto('#resultado', "Todos los amigos fueron sorteados");
+        asignarElementoTexto('#listaAmigos', "");
+    }   
+     let pos = amigos.indexOf(amigoSorteado);
+     let eleminarElemento = amigos.splice(sortearAmigo, 1);
+     actualizarListaAmigos();
+
     console.log(amigoSorteado);
+    console.log(amigos);
+
 }
 
-function limpiarCaja(){
-    document.querySelector(`#listaAmigos`).value = "";
+
+function actualizarListaAmigos() {
+    asignarElementoTexto('#listaAmigos', ""); //Limpio la lista anterior
+
+    for (let i = 0; i < amigos.length; i++) {
+        let nuevoElemento = document.createElement("li"); // Creo un nuevo elemento <li>
+        nuevoElemento.textContent = amigos[i]; // Asigno el nombre del amigo nuevo agregado
+        listaAmigos.appendChild(nuevoElemento); // Agrego el <li> a la lista
+    }
 }
-
-
 
